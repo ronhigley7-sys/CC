e0ad93daba76640e9b7c29f3d075ac2c89cde145
ronhigley7-sys <ron.higley7@gmail.com>
2026-09-15 17:12:27 -0400

    Refresh full-page staffing print

--- cc-init.js
+++ cc-init.js
@@ -21,7 +21,7 @@ if (new URLSearchParams(location.search).get('vf')) {
 // Admission rotation + 3C CA assignment patch.
 (function(){
   var s=document.createElement('script');
-  s.src='cc-admission-rotation.js?v=20260915-compact-one-page';
+  s.src='cc-admission-rotation.js?v=20260915-fill-full-page';
   s.async=false;
   document.head.appendChild(s);
 })();
